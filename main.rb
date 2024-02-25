3cd73a2412b53815117f940e705bdb77666a6ac2
gominie <109306648+gominie@users.noreply.com>
2024-02-25 21:43:56 +0000

    fix computer guess.length limit need update guess code remove duplicates after 1 iteration

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
-secret_code = [1, 2, 3, 4]
-my_guess = [2, 4, 6, 7]
+# idea only replace nil values with random after iterating thru values to find match
+# how to target the indexes that match??
+# arr_with-index/
 
 class Mastermind
   attr_reader :gen_code
+
   def initialize
     @secret_code = []
     @guess = []
@@ -12,73 +14,102 @@ class Mastermind
     @play_mode = 0
   end
 
-
   def create_secret_code
-    #numbers = (0..6).to_a
     @gen_code = (0..6).to_a.shuffle.take(4)
     @secret_code = @gen_code
-    #p "secret code is: #{@secret_code}"
+    # p "secret code is: #{@secret_code}"
   end
 
-  def victory?
-    if @guess == @secret_code
-      @victory = true
-      p "You have won!"
+  def create_computer_guess
+    empty_arr = Array.new(4)
+    puts "empty arr value before code is #{empty_arr}"
+    @guess.each_with_index do |num, index|
+      if @guess[index] == @secret_code[index]
+        empty_arr[index] = @secret_code[index]
+      elsif @secret_code.include?(num)
+        empty_indexes = empty_arr.each_index.select { |i| empty_arr[i].nil? }
+        random_empty_index = empty_indexes.sample
+        empty_arr[random_empty_index] = num
+      end
     end
+    empty_arr.map! {|value| value.nil? ? (0..6).to_a.sample : value}
+    @guess = empty_arr
+    puts "guess value after computer guess method is #{@guess}"
+  end
+
+
+  def victory?
+    return unless @guess == @secret_code
+
+    @victory = true
+    p 'You have won!'
   end
 
   def code_breaker
-    p "Play mode 2 starts"
+    p 'Play mode 2 starts'
     create_secret_code
     while !@victory && @guess_count < 6
-      puts "What's the code? #{6-@guess_count} guesses remaining"
+      puts "What's the code? #{6 - @guess_count} guesses remaining"
       @guess = gets.chomp.to_i.digits.reverse
       next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
+
       @guess_count += 1
       break if victory?
-      #puts "Guess Count: #{@guess_count}"
+
+      # puts "Guess Count: #{@guess_count}"
       handle_guess if @guess_count < 6
       puts 'Out of guesses' if @guess_count == 6 && !victory?
     end
   end
 
   def code_maker
-    puts "Please input code"
+    puts 'Please input code'
     create_secret_code
     @secret_code = gets.chomp.to_i.digits.reverse
-    @guess = @gen_code
-    p "guess is: #{@guess}"
     p "secret code is: #{@secret_code}"
-    #while !victory?
-    #@guess = (0..6).to_a.shuffle.take(4)
+    while !@victory && @guess_count < 6
+      if @guess_count == 0
+        @guess = (0..6).to_a.shuffle.take(4)
+        puts "guess is!!: #{@guess}"
+        victory?
+        handle_guess
+        @guess_count += 1
+      end
+      create_computer_guess
+      p "secret code is: #{@secret_code}"
+      p "guess is: #{@guess}"
+      @guess_count += 1
+      break if victory?
 
+      handle_guess if @guess_count < 6
+      puts 'Out of guesses' if @guess_count == 6 && !victory?
+    end
   end
 
   def handle_guess
-      hint_arr = []
-      @guess.each_with_index do |num, index|
+    hint_arr = []
+    @guess.each_with_index do |num, index|
       if @secret_code[index] == @guess[index]
-        @hint_arr << "●"
+        @hint_arr << '●'
       elsif @secret_code.include?(num)
-        @hint_arr << "○"
+        @hint_arr << '○'
       end
-     end
-     @hint_arr.shuffle!
-     p "hint: #{@hint_arr.join(" ")}"
-     @hint_arr = []
+    end
+    @hint_arr.shuffle!
+    p "hint: #{@hint_arr.join(' ')}"
+    @hint_arr = []
   end
-
 
   def start_game
     puts 'Welcome to terminal mastermind!'
-    puts "Press 1 to be a code maker and 2 to be a code breaker"
+    puts 'Press 1 to be a code maker and 2 to be a code breaker'
     @play_mode = gets.chomp.to_i
-    while !(@play_mode == 1 || @play_mode == 2)
-      puts "Invalid play mode. Please enter 1 or 2"
+    until @play_mode == 1 || @play_mode == 2
+      puts 'Invalid play mode. Please enter 1 or 2'
       @play_mode = gets.chomp.to_i
     end
     if @play_mode == 2
-    code_breaker
+      code_breaker
     elsif @play_mode == 1
       code_maker
     end
@@ -87,24 +118,3 @@ end
 
 game = Mastermind.new
 game.start_game
-
-#need put play modes in own methods
-
-=begin
-p "Play mode 2 starts"
-create_computer_guess
-while !@victory && @guess_count < 6
-  puts "What's the code? #{6-@guess_count} guesses remaining"
-  @guess = gets.chomp.to_i.digits.reverse
-  next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
-
-  @guess_count += 1
-  break if victory?
-
-  puts "Guess Count: #{@guess_count}"
-  handle_guess
-  puts 'Out of guesses' if @guess_count == 6 && !victory?
-end
-elsif @play_mode == 1
- p "need input code"
-=end
