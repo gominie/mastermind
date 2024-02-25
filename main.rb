313b7820f15c0d4acafd14e8346d7324bb83a4df
gominie <109306648+gominie@users.noreply.com>
2024-02-25 13:53:06 +0000

    add guess count down

--- main.rb
+++ main.rb
@@ -8,10 +8,18 @@ class Mastermind
     @victory = false
     @guess_count = 0
     @hint_arr = []
+    @play_mode = 0
+  end
+
+  def play_mode
+    p 'Select player mode'
+    p 'Press 1 for code maker or 2 for code breaker'
+    @play_mode = gets.chomp.to_i
+    p "play mode = #{@play_mode}"
   end
 
   def create_computer_guess
-    numbers = (1..6).to_a
+    numbers = (0..6).to_a
     @secret_code = numbers.shuffle.take(4)
     #p "secret code is: #{@secret_code}"
   end
@@ -39,20 +47,25 @@ class Mastermind
 
 
   def start_game
-    @secret_code = [1, 2, 4, 4]
-    puts "Welcome to terminal mastermind!"
-    create_computer_guess
-    while (!@victory && @guess_count <= 6)
-      result = !@victory || @guess_count <= 6
-      @guess = gets.chomp.to_i.digits.reverse
-      next unless @guess.length == 4 && @guess.all? {|num| (1..6).include?(num)}
-      @guess_count += 1
-      break if victory?
-      p "Guess Count: #{@guess_count}"
-      handle_guess
-      if @guess_count == 6 && !victory?
-        p "Out of guesses"
-       end
+    puts 'Welcome to terminal mastermind!'
+    play_mode
+    if @play_mode == 2
+      p "play mode 2 starts"
+      create_computer_guess
+      while !@victory && @guess_count < 6
+        p "What's the code? #{6-@guess_count} guesses remaining"
+        @guess = gets.chomp.to_i.digits.reverse
+        next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
+
+        @guess_count += 1
+        break if victory?
+
+        p "Guess Count: #{@guess_count}"
+        handle_guess
+        p 'Out of guesses' if @guess_count == 6 && !victory?
+      end
+      elsif @play_mode == 1
+       p "need input code"
     end
   end
 end
