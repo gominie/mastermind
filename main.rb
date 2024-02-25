6b19153fdcda764bc4042636f7bf39af6cefaf07
gominie <109306648+gominie@users.noreply.com>
2024-02-25 13:22:24 +0000

    Rename testing.rb to main.rb

--- main.rb
+++ main.rb
@@ -11,7 +11,9 @@ class Mastermind
   end
 
   def create_computer_guess
-    # fill in later
+    numbers = (1..6).to_a
+    @secret_code = numbers.shuffle.take(4)
+    #p "secret code is: #{@secret_code}"
   end
 
   def victory?
@@ -22,43 +24,42 @@ class Mastermind
   end
 
   def handle_guess
-    hint_arr = []
-    @guess.each_with_index do |num, index|
+      hint_arr = []
+      @guess.each_with_index do |num, index|
       if @secret_code[index] == @guess[index]
         @hint_arr << "●"
       elsif @secret_code.include?(num)
         @hint_arr << "○"
       end
-    end
-    @hint_arr.shuffle!
-    p "hint: #{@hint_arr.join(" ")}"
-    @hint_arr = []
+     end
+     @hint_arr.shuffle!
+     p "hint: #{@hint_arr.join(" ")}"
+     @hint_arr = []
   end
 
 
   def start_game
-    @secret_code = [1, 2, 3, 4]
+    @secret_code = [1, 2, 4, 4]
     puts "Welcome to terminal mastermind!"
+    create_computer_guess
     while (!@victory && @guess_count <= 6)
       result = !@victory || @guess_count <= 6
       @guess = gets.chomp.to_i.digits.reverse
-      p @guess.length
-      next unless @guess.length == 4
-      break if victory?
-      handle_guess
+      next unless @guess.length == 4 && @guess.all? {|num| (1..6).include?(num)}
       @guess_count += 1
-      p " Guess Count: #{@guess_count}"
+      break if victory?
+      p "Guess Count: #{@guess_count}"
+      handle_guess
       if @guess_count == 6 && !victory?
-        p "No win"
-    end
+        p "Out of guesses"
+       end
     end
   end
 end
-
-#only allow 4 guesses
 
 game = Mastermind.new
 game.start_game
 
 #added no victory case
 #wat happens i correct on 6th guess
+#all array for 1-6
