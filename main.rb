3a450f0599eac8249f782b1f89fac5bcc9fb3939
gominie <109306648+gominie@users.noreply.com>
2024-02-25 15:11:13 +0000

    add computer guess

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ secret_code = [1, 2, 3, 4]
 my_guess = [2, 4, 6, 7]
 
 class Mastermind
+  attr_reader :gen_code
   def initialize
     @secret_code = []
     @guess = []
@@ -11,16 +12,11 @@ class Mastermind
     @play_mode = 0
   end
 
-  def play_mode
-    p 'Select player mode'
-    p 'Press 1 for code maker or 2 for code breaker'
-    @play_mode = gets.chomp.to_i
-    p "play mode = #{@play_mode}"
-  end
 
-  def create_computer_guess
-    numbers = (0..6).to_a
-    @secret_code = numbers.shuffle.take(4)
+  def create_secret_code
+    #numbers = (0..6).to_a
+    @gen_code = (0..6).to_a.shuffle.take(4)
+    @secret_code = @gen_code
     #p "secret code is: #{@secret_code}"
   end
 
@@ -29,6 +25,33 @@ class Mastermind
       @victory = true
       p "You have won!"
     end
+  end
+
+  def code_breaker
+    p "Play mode 2 starts"
+    create_secret_code
+    while !@victory && @guess_count < 6
+      puts "What's the code? #{6-@guess_count} guesses remaining"
+      @guess = gets.chomp.to_i.digits.reverse
+      next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
+      @guess_count += 1
+      break if victory?
+      #puts "Guess Count: #{@guess_count}"
+      handle_guess if @guess_count < 6
+      puts 'Out of guesses' if @guess_count == 6 && !victory?
+    end
+  end
+
+  def code_maker
+    puts "Please input code"
+    create_secret_code
+    @secret_code = gets.chomp.to_i.digits.reverse
+    @guess = @gen_code
+    p "guess is: #{@guess}"
+    p "secret code is: #{@secret_code}"
+    #while !victory?
+    #@guess = (0..6).to_a.shuffle.take(4)
+
   end
 
   def handle_guess
@@ -48,24 +71,16 @@ class Mastermind
 
   def start_game
     puts 'Welcome to terminal mastermind!'
-    play_mode
+    puts "Press 1 to be a code maker and 2 to be a code breaker"
+    @play_mode = gets.chomp.to_i
+    while !(@play_mode == 1 || @play_mode == 2)
+      puts "Invalid play mode. Please enter 1 or 2"
+      @play_mode = gets.chomp.to_i
+    end
     if @play_mode == 2
-      p "play mode 2 starts"
-      create_computer_guess
-      while !@victory && @guess_count < 6
-        p "What's the code? #{6-@guess_count} guesses remaining"
-        @guess = gets.chomp.to_i.digits.reverse
-        next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
-
-        @guess_count += 1
-        break if victory?
-
-        p "Guess Count: #{@guess_count}"
-        handle_guess
-        p 'Out of guesses' if @guess_count == 6 && !victory?
-      end
-      elsif @play_mode == 1
-       p "need input code"
+    code_breaker
+    elsif @play_mode == 1
+      code_maker
     end
   end
 end
@@ -73,6 +88,23 @@ end
 game = Mastermind.new
 game.start_game
 
-#added no victory case
-#wat happens i correct on 6th guess
-#all array for 1-6
+#need put play modes in own methods
+
+=begin
+p "Play mode 2 starts"
+create_computer_guess
+while !@victory && @guess_count < 6
+  puts "What's the code? #{6-@guess_count} guesses remaining"
+  @guess = gets.chomp.to_i.digits.reverse
+  next unless @guess.length == 4 && @guess.all? { |num| (0..6).include?(num) }
+
+  @guess_count += 1
+  break if victory?
+
+  puts "Guess Count: #{@guess_count}"
+  handle_guess
+  puts 'Out of guesses' if @guess_count == 6 && !victory?
+end
+elsif @play_mode == 1
+ p "need input code"
+=end
